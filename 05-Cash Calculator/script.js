10bea600d56ea7ee50d756466da670ce152c1df3
asanchez89 <idrian89@gmail.com>
2024-04-11 12:39:10 -0400

    Implementacion logica numero a texto calculadora efectivo

--- 05-Cash Calculator/script.js
+++ 05-Cash Calculator/script.js
@@ -47,5 +47,73 @@ document.addEventListener("DOMContentLoaded",()=>{
             totalCashValue += parseInt(text.textContent);
         });
         txtFinalCash.textContent = "Total Cash: "+totalCashValue;
+        txtFinalCashInWords.textContent = `Total Cash In Words: ${convertToWords(totalCashValue)}`;
+    }
+
+    btnReset.addEventListener('click',clearData);
+    function clearData(){
+        cashInputs.forEach((input)=>{
+            input.value = "";
+        })
+        cashTexts.forEach((text)=>{
+            text.textContent = "0";
+        })
+        totalCash();
+    }
+
+    cashInputs.forEach(input=>{
+        input.addEventListener("input",()=>{
+            const value = parseInt(input.value, 10);
+            if(isNaN(value)||value<0){
+                input.value = "";
+            }
+        });
+    });
+
+    function convertToWords(number){
+        const units = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+        const teens = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+        const tens = ['','','Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+
+        if(number === 0){
+            return 'Zero';
+        }
+
+        let words = '';
+
+        if(Math.floor(number / 10000000) > 0){
+            words += convertToWords(Math.floor(number / 10000000)) + ' Crore ';
+            number %= 10000000;
+        }
+
+        if(Math.floor(number / 100000) > 0){
+            words += convertToWords(Math.floor(number / 100000)) + ' Lakh ';
+            number %= 100000;
+        }
+
+        if(Math.floor(number / 1000) > 0){
+            words += convertToWords(Math.floor(number / 1000)) + ' Thousand ';
+            number %= 1000;
+        }
+
+        if(Math.floor(number / 100) > 0){
+            words += convertToWords(Math.floor(number / 100)) + ' Hundred ';
+            number %= 100;
+        }
+
+        if(number > 0){
+            if(number < 10){
+                words += units[number];
+            }else if(number < 20){
+                words += teens[number - 10];
+            }else{
+                words += tens[Math.floor(number/10)];
+                if(number % 10 > 0){
+                    words += ' '+ units[number%10];
+                }
+            }
+        }
+
+        return words.trim();
     }
 });
